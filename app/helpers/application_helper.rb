8ad389b1141809d92dc8d999d874f92c0be6e92d
Jon Moorman <jmoorman@calpoly.edu>
2010-07-22 15:37:45 -0700

    ch5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
          "#{base_title} | #{@title}"
       end
    end
+
+   #return the logo for the application
+   def logo
+      image_tag('logo.png', :alt => 'Sample App', :class => 'round')
+   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
+require 'webrat'
 
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
@@ -11,6 +12,10 @@ require 'spec/rails'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+Webrat.configure do |config|
+   config.mode = :rails
+end
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
