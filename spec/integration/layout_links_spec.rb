f6e69afb52f2291a3f5cc56146229012866c6766
Jon Moorman <jmoorman@calpoly.edu>
2010-07-26 23:24:47 -0700

    fixed typo

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -59,7 +59,7 @@ describe "LayoutLinks" do
 
       it "should have a signout link" do
          visit root_path
-         response.should have_tag("a[href=?", signout_path, "Sign out")
+         response.should have_tag("a[href=?]", signout_path, "Sign out")
       end
 
       it "should have a profile link" do
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -50,7 +50,7 @@ describe "Users" do
             user = Factory(:user)
             visit signin_path
             fill_in :email,      :with => user.email
-            fill_in :password    :with => user.password
+            fill_in :password,    :with => user.password
             click_button
             controller.should be_signed_in
             click_link "Sign out"
